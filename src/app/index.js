4f94f68b51411f87f3f93e6185abf5dc3c14145c
Ukkiez <yannick_3d@hotmail.com>
2023-01-08 16:56:40 +0100

    stop HTML drag-and-drop behaviour

--- src/app/index.js
+++ src/app/index.js
@@ -4,4 +4,16 @@
 // (package.json) but that doesn't appear to work
 nw.Window.get().setShadow( false ); // eslint-disable-line no-undef
 
+// prevent drag-and-drop behaviour (e.g. dragging an HTML file into the app,
+// which by default would display that page instead)
+document.body.addEventListener( "dragover", function( e ){
+  e.preventDefault();
+  e.stopPropagation();
+}, false );
+
+document.body.addEventListener( "drop", function( e ){
+  e.preventDefault();
+  e.stopPropagation();
+}, false );
+
 import "./initialize.js";
