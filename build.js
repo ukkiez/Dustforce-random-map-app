f94a681daf0bebe08ef04b9dcc3da93ea825e90a
Ukkiez <yannick_3d@hotmail.com>
2024-06-17 22:11:56 +0200

    adjust building

--- build.js
+++ build.js
@@ -46,6 +46,8 @@ async function copyDir( src, dest, exclusions = [] ) {
   }
 }
 
+const unixDirs = [ "dist/RMA_osx64/", "dist/RMA_linux32/", "dist/RMA_linux64/" ];
+
 // build Mac and Linux versions
 const nw = new NwBuilder( {
   files: [ "./src/**/*" ],
@@ -56,7 +58,7 @@ const nw = new NwBuilder( {
   forceDownload: false,
   buildDir: "./dist",
   appName: "random-map-app",
-  appVersion: "0.0.1",
+  appVersion: "1.0.0",
   macIcns: "./src/assets/s-complete-icon.icns",
   winIco: "./src/assets/s-complete-icon.icns",
 } );
@@ -80,29 +82,36 @@ const modulesPath = "./src/node_modules";
   await fsPromises.copyFile( "./src/settings/default-settings.json", "./src/user-data/settings.json" );
   await fsPromises.copyFile( "./src/settings/default-configuration.json", "./src/user-data/configuration.json" );
 
+  // TODO: overwrite the personal bests
+
   await nw.build();
   console.log( "> Built Mac&Linux" );
 
   // rename directories to a clearer name for the user
-  await fsPromises.rename( "dist/random-map-app/osx64", "dist/random-map-app_osx64" );
-  await fsPromises.rename( "dist/random-map-app/linux32", "dist/random-map-app_linux32" );
-  await fsPromises.rename( "dist/random-map-app/linux64", "dist/random-map-app_linux64" );
+  await fsPromises.rename( "dist/random-map-app/osx64", "dist/RMA_osx64" );
+  await fsPromises.rename( "dist/random-map-app/linux32", "dist/RMA_linux32" );
+  await fsPromises.rename( "dist/random-map-app/linux64", "dist/RMA_linux64" );
 
   // remove the leftover directory
   await fsPromises.rmdir( "dist/random-map-app" );
 
-  // create a zip file for distribution; note that we CD into the "dist"
-  // directory first so the zip archive doesn't include the folder
-  await execAsync( "cd dist && zip -ry RMA-osx64.zip random-map-app_osx64/random-map-app.app", true );
-  await execAsync( "cd dist && zip -ry RMA-linux32.zip random-map-app_linux32", true );
-  await execAsync( "cd dist && zip -ry RMA-linux64.zip random-map-app_linux64", true );
-  console.log( "> Compressed Mac&Linux" );
+  // remove useless credits html
+  for ( const dir of unixDirs ) {
+    await fsPromises.rm( dir + "credits.html" );
+  }
+
+  // // create a zip file for distribution; note that we CD into the "dist"
+  // // directory first so the zip archive doesn't include the folder
+  // await execAsync( "cd dist && zip -ry RMA-osx64.zip random-map-app_osx64/random-map-app.app", true );
+  // await execAsync( "cd dist && zip -ry RMA-linux32.zip random-map-app_linux32", true );
+  // await execAsync( "cd dist && zip -ry RMA-linux64.zip random-map-app_linux64", true );
+  // console.log( "> Compressed Mac&Linux" );
 } )().then( async () => {
   const buildWindows = async () => {
     // construct the Windows build manually, since this version of nw-builder
     // can't do it, and newer versions don't actually properly function; note
     // that this assumes the NW binaries have been downloaded and put in the
-    // cache beforehand
+    // cache folder beforehand
     const buildPath = "./dist/win32";
     const srcPath = "./src";
     const nwBinaries = "./cache/0.52.2-normal/win32";
@@ -141,11 +150,11 @@ const modulesPath = "./src/node_modules";
     // fs.rmSync( "./temp", { recursive: true } );
 
     // rename directories to a clearer name for the user
-    await fsPromises.rename( "dist/win32", "dist/random-map-app_win32" );
+    await fsPromises.rename( "dist/win32", "dist/RMA_win32" );
 
-    // create a zip file for distribution; note that we CD into the "dist"
-    // directory first so the zip archive doesn't include the folder
-    await execAsync( "cd dist && zip -ry RMA-win32.zip random-map-app_win32", true );
+    // // create a zip file for distribution; note that we CD into the "dist"
+    // // directory first so the zip archive doesn't include the folder
+    // await execAsync( "cd dist && zip -ry RMA-win32.zip random-map-app_win32", true );
     console.log( "> Built Windows" );
   }
 
--- jsonToBinary.js
+++ jsonToBinary.js
@@ -4,7 +4,7 @@ const defaultSettingsPath = "./src/settings/";
 
 const modes = JSON.stringify( require( "./src/settings/modes.json" ) );
 const defaultSettings = JSON.stringify( require( "./src/settings/default-settings.json" ) );
-const defaultPB = JSON.stringify( require( "./src/settings/default-personal-bests.json" ) );
+const defaultPB = JSON.stringify( require( "./src/user-data/personal-bests.json" ) );
 const filteredMetadata = JSON.stringify( require( "./src/dustkid-data/filtered-metadata.json" ) );
 
 const modesB = Buffer.from( modes ).toString( "hex" );
