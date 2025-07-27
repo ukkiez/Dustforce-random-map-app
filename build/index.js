9ad4a25feef433764fdfd4c3aaaca7b31aec042b
ukkiez <yannick_3d@hotmail.com>
2025-07-27 14:45:22 +0200

    refactor build process into new directory

--- build/index.js
+++ build/index.js
@@ -4,6 +4,7 @@ const fs = require( "fs" );
 const { promises: fsPromises } = fs;
 
 const path = require( "path" );
+const { join } = path;
 
 const { exec } = require( "child_process" );
 
@@ -46,58 +47,84 @@ async function copyDir( src, dest, exclusions = [] ) {
   }
 }
 
-const unixDirs = [ "dist/RMA_osx64/", "dist/RMA_linux32/", "dist/RMA_linux64/" ];
+async function pathExists( path ) {
+  try {
+    await fsPromises.stat( path );
+    return true;
+  }
+  catch ( error ) {
+    return false;
+  }
+}
+
+async function removeIfExists( path, recursive = false ) {
+  if ( await pathExists( path ) ) {
+    await fsPromises.rm( path, { recursive } );
+
+    return true;
+  }
+
+  return false;
+}
+
+const PATHS = {
+  ROOT: path.resolve( `${ __dirname }/..` ),
+  BUILD: path.resolve( __dirname ),
+};
+
+PATHS.SRC = join( PATHS.ROOT, "src" );
+PATHS.DIST = join( PATHS.BUILD, "dist" );
+PATHS.DEFAULTS = join( PATHS.BUILD, "defaults" );
+PATHS.USER_DATA = join( PATHS.SRC, "user-data" );
+
+const unixDirs = [ join( PATHS.DIST, "RMA_osx64" ), join( PATHS.DIST, "RMA_linux32" ), join( PATHS.DIST, "RMA_linux64" ) ];
 
 // build Mac and Linux versions
 const nw = new NwBuilder( {
-  files: [ "./src/**/*" ],
+  files: [ join( PATHS.SRC, "**/*" ) ],
   version: "0.52.2",
   flavor: "normal",
   platforms: [ "osx64", "linux32", "linux64" ],
-  cacheDir: "./cache",
+  cacheDir: join( PATHS.BUILD, "cache" ),
   forceDownload: false,
-  buildDir: "./dist",
+  buildDir: PATHS.DIST,
   appName: "random-map-app",
-  appVersion: "1.0.0",
-  macIcns: "./src/assets/s-complete-icon.icns",
-  winIco: "./src/assets/s-complete-icon.icns",
+  appVersion: "1.0.2",
+  macIcns: join( PATHS.SRC, "assets/s-complete-icon.icns" ),
+  winIco: join( PATHS.SRC, "assets/s-complete-icon.icns" ),
+  downloadUrl: "https://dl.node-webkit.org/",
 } );
 
-const modulesPath = "./src/node_modules";
+const modulesPath = join( PATHS.SRC, "node_modules" );
 ( async () => {
-  try {
-    // remove node modules (since we only use dev modules anyway)
-    fs.rmSync( modulesPath, { recursive: true } );
-  }
-  catch ( e ) {
-    console.error( e );
-  }
+  // remove node modules since we only use dev modules anyway
+  await removeIfExists( modulesPath, recursive = true );
 
   // clear out the dist folder
-  await fsPromises.rm( "./dist/", { recursive: true } );
+  await removeIfExists( PATHS.DIST, recursive = true );
 
   // overwrite the user settings and configuration with the defaults
-  await fsPromises.rm( "./src/user-data/settings.json" );
-  await fsPromises.rm( "./src/user-data/configuration.json" );
-  await fsPromises.copyFile( "./src/settings/default-settings.json", "./src/user-data/settings.json" );
-  await fsPromises.copyFile( "./src/settings/default-configuration.json", "./src/user-data/configuration.json" );
-
-  // TODO: overwrite the personal bests
+  await removeIfExists( join( PATHS.USER_DATA, "settings.json" ) );
+  await removeIfExists( join( PATHS.USER_DATA, "configuration.json" ) );
+  await removeIfExists( join( PATHS.USER_DATA, "personal-bests.bin" ) );
+  await fsPromises.copyFile( join( PATHS.DEFAULTS, "settings.bin" ), join( PATHS.USER_DATA, "settings.bin" ) );
+  await fsPromises.copyFile( join( PATHS.DEFAULTS, "configuration.json" ), join( PATHS.USER_DATA, "configuration.json" ) );
+  await fsPromises.copyFile( join( PATHS.DEFAULTS, "personal-bests.bin" ), join( PATHS.USER_DATA, "personal-bests.bin" ) );
 
   await nw.build();
   console.log( "> Built Mac&Linux" );
 
   // rename directories to a clearer name for the user
-  await fsPromises.rename( "dist/random-map-app/osx64", "dist/RMA_osx64" );
-  await fsPromises.rename( "dist/random-map-app/linux32", "dist/RMA_linux32" );
-  await fsPromises.rename( "dist/random-map-app/linux64", "dist/RMA_linux64" );
+  await fsPromises.rename( join( PATHS.DIST, "random-map-app/osx64" ), join( PATHS.DIST, "RMA_osx64" ) );
+  await fsPromises.rename( join( PATHS.DIST, "random-map-app/linux32" ), join( PATHS.DIST, "RMA_linux32" ) );
+  await fsPromises.rename( join( PATHS.DIST, "random-map-app/linux64" ), join( PATHS.DIST, "RMA_linux64" ) );
 
   // remove the leftover directory
-  await fsPromises.rmdir( "dist/random-map-app" );
+  await removeIfExists( join( PATHS.DIST, "random-map-app" ), recursive = true );
 
   // remove useless credits html
   for ( const dir of unixDirs ) {
-    await fsPromises.rm( dir + "credits.html" );
+    await removeIfExists( join( dir, "credits.html" ) );
   }
 
   // // create a zip file for distribution; note that we CD into the "dist"
@@ -106,15 +133,15 @@ const modulesPath = "./src/node_modules";
   // await execAsync( "cd dist && zip -ry RMA-linux32.zip random-map-app_linux32", true );
   // await execAsync( "cd dist && zip -ry RMA-linux64.zip random-map-app_linux64", true );
   // console.log( "> Compressed Mac&Linux" );
-} )().then( async () => {
+} )()
+.then( async () => {
   const buildWindows = async () => {
     // construct the Windows build manually, since this version of nw-builder
     // can't do it, and newer versions don't actually properly function; note
     // that this assumes the NW binaries have been downloaded and put in the
     // cache folder beforehand
-    const buildPath = "./dist/win32";
-    const srcPath = "./src";
-    const nwBinaries = "./cache/0.52.2-normal/win32";
+    const buildPath = join( PATHS.DIST, "win32" );
+    const nwBinaries = join( PATHS.BUILD, "cache/0.52.2-normal/win32" );
     try {
       // remove any existing build
       fs.rmSync( buildPath, { recursive: true } );
@@ -122,9 +149,9 @@ const modulesPath = "./src/node_modules";
     catch ( e ) {}
 
     fs.mkdirSync( buildPath );
-    fs.mkdirSync( path.join( buildPath, "package.nw" ) );
+    fs.mkdirSync( join( buildPath, "package.nw" ) );
     await copyDir( nwBinaries, buildPath );
-    await copyDir( srcPath, path.join( buildPath, "/package.nw" ), [
+    await copyDir( PATHS.SRC, join( buildPath, "/package.nw" ), [
       "node_modules",
       ".eslintrc.json",
       "package-lock.json",
@@ -137,20 +164,20 @@ const modulesPath = "./src/node_modules";
     //   fs.rmSync( "./temp", { recursive: true } );
     // }
     // fs.mkdirSync( "./temp" );
-    // fs.copyFileSync( path.join( srcPath, "/package.json" ), "./temp/package.json" );
+    // fs.copyFileSync( join( PATHS.SRC, "/package.json" ), "./temp/package.json" );
 
     // await execAsync( "npm i --omit=dev --prefix ./temp", true );
 
     // await copyDir( "./temp/node_modules", buildPath + "/node_modules" );
 
     // rename nw.exe
-    await fsPromises.rename( path.join( buildPath, "nw.exe" ), path.join( buildPath, "random-map-app.exe" ) );
+    await fsPromises.rename( join( buildPath, "nw.exe" ), join( buildPath, "random-map-app.exe" ) );
 
     // // clean up
     // fs.rmSync( "./temp", { recursive: true } );
 
     // rename directories to a clearer name for the user
-    await fsPromises.rename( "dist/win32", "dist/RMA_win32" );
+    await fsPromises.rename( buildPath, join( PATHS.DIST, "RMA_win32" ) );
 
     // // create a zip file for distribution; note that we CD into the "dist"
     // // directory first so the zip archive doesn't include the folder
@@ -161,5 +188,8 @@ const modulesPath = "./src/node_modules";
   if ( process.argv.includes( "win" ) || process.argv.includes( "windows" ) ) {
     await buildWindows();
   }
+} )
+.catch( error => {
+  console.log( error );
 } );
 
